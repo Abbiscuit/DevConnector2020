0020bc8cda1b27d16c2b747f7a180793d7a1c8af
Hiroshi Kadokura <abbiscuit.com@gmail.com>
2019-12-09 00:54:39 +0900

    Delete profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -150,4 +150,25 @@ router.get("/user/:user_id", async (req, res) => {
   }
 });
 
+// @route   DELETE api/profile
+// @desc    Delete Profile, User & Posts
+// @access  Private
+router.delete("/", auth, async (req, res) => {
+  try {
+    // @todo - remove users
+
+    // Remove profile
+    await Profile.findOneAndDelete({ user: req.user.id });
+
+    // Remove user
+    await User.findOneAndDelete({ _id: req.user.id });
+
+    res.json({ msg: "User Deleted" });
+  } catch (err) {
+    console.error(err.message);
+
+    res.status(500).send("Server Error");
+  }
+});
+
 module.exports = router;
